d20a3e6115adeda7e9289cc3c0985c3c1f589333
taforyou <taforyou@hotmail.com>
2018-02-01 13:40:35 +0700

    Edit App.js Edit index.js
    Init firebase in index.js
    แก้ App.js นิดน่อย

--- src/index.js
+++ src/index.js
@@ -3,6 +3,18 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
+import * as firebase from 'firebase';
+
+var config = {
+    apiKey: "AIzaSyCi0xKqW0py3rz83QiLMd2M4Ab4DovcYio",
+    authDomain: "codecamp-week6.firebaseapp.com",
+    databaseURL: "https://codecamp-week6.firebaseio.com",
+    projectId: "codecamp-week6",
+    storageBucket: "codecamp-week6.appspot.com",
+    messagingSenderId: "55960876635"
+};
+
+firebase.initializeApp(config);
 
 ReactDOM.render(<App />, document.getElementById('root'));
 registerServiceWorker();
